6a3b66c39fcd31c5898cb97b8bea1920ef33fb63
alfaizuna <alfaizunaulia@gmail.com>
2024-11-28 22:18:19 +0700

    Initial commit

---
+++ src/app/index.tsx
@@ -0,0 +1,15 @@
+import { StyleSheet, Text, View } from 'react-native';
+
+const HomeScreen = () => {
+    return (
+        <View>
+            <Text>
+                Home
+            </Text>
+        </View>
+    )
+}
+
+export default HomeScreen;
+
+const styles = StyleSheet.create({})
